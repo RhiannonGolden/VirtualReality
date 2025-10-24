a05679997331b38c27f95490c7126a8faca975f1
RhiannonGolden <rhiannong7@nycstudents.net>
2025-10-24 17:35:19 +0000

    save

--- VRLesson2_5bClasswork/script.js
+++ VRLesson2_5bClasswork/script.js
@@ -1,5 +1,5 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene, snowmen = [ ];
+let scene, snowmen = [ ], clouds = [ ], snowflakes = [ ];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); //CSS Selector
@@ -19,9 +19,26 @@ window.addEventListener("DOMContentLoaded",function() {
   }
 
   //Challenge 1: Create an array to store 20 clouds in random positions
-  cloud = new Cloud(-5,10,-1);
+  for(let i = 0; i < 20; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    let c = new Cloud(x, 20, z);
+    clouds.push(c);
+  }
 
   //Challenge 3: Create an array to store 100 snowflakes in random positions
+  for(let i =0; i < 100; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    let s = new Snowflake(x, 20, z);
+    snowflakes.push(s)
+  }
+
+
+
+
+
+
 
   loop();
 })
@@ -30,10 +47,18 @@ function loop(){
   for(let snowman of snowmen){
     snowman.spin();
   }
-  cloud.fly();
+
   //Challenge 2: Traverse your array of clouds and make each cloud fly
+  for(let i=0; i<clouds.length;i++){
+    clouds[i].fly();
+  }
+
+
+  for(let i=0; i<snowflakes.length;i++){
+    snowflakes[i].fall();
+  }
 
   //Challenge 4: Traverse your array of snowflakes and make each snowflake fall
-  
+
   window.requestAnimationFrame( loop );
 }
