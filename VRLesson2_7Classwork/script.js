7a6de8630e0a32810fc9c18b138b923480ada42d
RhiannonGolden <rhiannong7@nycstudents.net>
2025-11-01 19:16:49 +0000

    save

--- VRLesson2_7Classwork/script.js
+++ VRLesson2_7Classwork/script.js
@@ -1,16 +1,29 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene, dudeTemplate;
+let scene, dudeTemplate, dudes = [ ];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   dudeTemplate = document.querySelector("#dudeTemplate");
 
   //Challenge 4: Create an array of dudes at random locations.
+  for(let i = 0; i < 50; i++){
+    let x = rnd(-50, 50);
+    let y = 1;
+    let z = rnd(-50, 50);
+    let dude = new Dude(x, y, z);
+    dudes.push(dude);
+  }
+
+
   
   loop();  
 })
 function loop(){
   //Challenge 5: Make all the dudes jump.
+
+  for(let i=0; i<dudes.length;i++){
+    dudes[i].jump();
+  }
   
   window.requestAnimationFrame( loop );
 }
