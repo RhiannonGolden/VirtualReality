11e4d085d5e921704c949179eb5acb582d447adb
RhiannonGolden <rhiannong7@nycstudents.net>
2025-12-16 17:28:14 +0000

    save

---
+++ VRLesson4_4Classwork/script.js
@@ -0,0 +1,27 @@
+let rnd = (l,u) => Math.random()*(u-l)+l;
+let scene, boxes = [];
+window.onload = function(){
+  scene = document.querySelector("a-scene");
+  /* Challenge
+     Create 10 random boxes through the world
+  */
+ for(let i = 0; i<10; i++){
+  let x = rnd(-20, 20);
+  let z = rnd(-20,20);
+  boxes.push( new Box(x,1,z) );
+ }
+ 
+}
+
+
+function loop(){
+  /* Challenge 
+     Make the boxes blast off
+  */
+  for(let box of boxes){
+    box.blast();
+  }
+    
+  window.requestAnimationFrame( loop )
+  
+}
